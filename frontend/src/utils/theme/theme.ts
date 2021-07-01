6531d71ba8af73c5a46fcb0dd74baa18488a79e1
Oleh <oleh.m97@gmail.com>
2021-07-01 22:41:46 +0300

    iniy

---
+++ frontend/src/utils/theme/theme.ts
@@ -0,0 +1,12 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: 'Roboto'
+    },
+    palette: {
+        
+    }
+})
+
+export default  theme;
