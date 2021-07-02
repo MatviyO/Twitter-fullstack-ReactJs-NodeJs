2dff9812d91e1c409e572dda91607a35b8caa6dc
Oleh <oleh.m97@gmail.com>
2021-07-02 17:25:19 +0300

    up

--- frontend/src/utils/theme/theme.ts
+++ frontend/src/utils/theme/theme.ts
@@ -1,12 +1,27 @@
 import {createMuiTheme} from '@material-ui/core/styles';
+import {red} from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
     typography: {
         fontFamily: 'Roboto'
     },
     palette: {
-        
-    }
+        primary: {
+            main: 'rgb(29, 161, 242)',
+            dark: 'rgb(26, 145, 218)',
+            contrastText: '#fff'
+        },
+        secondary: {
+            main: 'rgb(26, 145, 218)'
+        },
+        error: {
+            main: red.A400
+        },
+        background: {
+            default: '#fff'
+        },
+    },
+    shadows:  ['none'],
 })
 
 export default  theme;
