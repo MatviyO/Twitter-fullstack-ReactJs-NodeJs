cf77a939dc981e9f09c6e83d5fd1354855de6f1c
Oleh <oleh.m97@gmail.com>
2021-07-05 23:17:04 +0300

    up

--- frontend/src/components/dialog/Dialog.tsx
+++ frontend/src/components/dialog/Dialog.tsx
@@ -6,70 +6,54 @@ import {
     DialogTitle,
     FormControl,
     FormGroup,
-    IconButton,
+    IconButton, makeStyles,
     TextField
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        fontWeight: 700,
+    },
+    lightBulb: {
+        verticalAlign: 'middle',
+        marginRight: theme.spacing(1)
+    },
+}))
+
 interface Props {
     title: string;
     children: ReactNode;
+    open: boolean;
+    onClose: () => void
 }
 
-const DialogComponent: FC<Props> = ({title, children}: Props): ReactElement => {
 
-    const [open, setOpen] = useState(false)
 
-    const handleClickOpen = () => {
-        setOpen(!open)
+const DialogComponent: FC<Props> = ({title, children, open = false, onClose}: Props): ReactElement | null => {
+    const classes = useStyles()
+
+    if (!open) {
+        return null;
     }
 
     return (
         <Dialog
             open={open}
-            onClose={handleClickOpen}
+            onClose={onClose}
             aria-labelledby="max-width-dialog-title"
         >
             <DialogTitle id="max-width-dialog-title">
-                <IconButton onClick={handleClickOpen}
+                <IconButton onClick={onClose}
                             color="secondary"
                             aria-label="clsoe">
                     <CloseIcon style={{fontSize: 26}}/>
                 </IconButton>
-                Login to Twitter
+                {title}
             </DialogTitle>
             <DialogContent>
                 <form noValidate>
-                    <FormControl component="fieldset" fullWidth>
-                        <FormGroup aria-label="position" row>
-                            <TextField
-                                autoFocus
-                                margin="dense"
-                                id="email"
-                                label="Enter your Email"
-                                type="email"
-                                InputLabelProps={{shrink: true}}
-                                variant="filled"
-                                fullWidth
-                                className={classes.loginSideField}
-                            />
-                            <TextField
-                                className={classes.loginSideField}
-                                autoFocus
-                                margin="dense"
-                                id="password"
-                                label="Enter your Password"
-                                type="password"
-                                InputLabelProps={{shrink: true}}
-                                variant="filled"
-                                fullWidth
-                            />
-                            <Button onClick={handleClickOpen} variant="contained" color="primary" fullWidth>
-                                Login
-                            </Button>
-                        </FormGroup>
-                    </FormControl>
-
+                    {children}
                 </form>
             </DialogContent>
         </Dialog>
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,25 +1,23 @@
 import React, {useState} from 'react';
-import {makeStyles,} from "@material-ui/core";
+import {Button, FormControl, FormGroup, makeStyles, TextField,} from "@material-ui/core";
 import TwitterIcon from '@material-ui/icons/Twitter';
 import SectionTitle from "../components/title/SectionTitle";
+import DialogComponent from "../components/dialog/Dialog";
+
 
 const useStyles = makeStyles((theme) => ({
-    button: {
-        fontWeight: 700,
-    },
-    lightBulb: {
-        verticalAlign: 'middle',
-        marginRight: theme.spacing(1)
-    },
     loginSideField: {
         marginButtom: 18
     }
 }))
 
 const SignIn = () => {
-
     const classes = useStyles()
+    const [open, setOpen] = useState(false)
 
+    const handleClickOpen = () => {
+        setOpen(!open)
+    }
     return (
         <div className="container_auth">
             <section className="wrapper__left">
@@ -31,7 +29,7 @@ const SignIn = () => {
                             <TwitterIcon fontSize="large" className="main__icon_twitt"/>
                         </div>
                         <div className="main__title">
-                            <SectionTitle  title="Here and now"/>
+                            <SectionTitle title="Here and now"/>
                         </div>
                         <div className="main__description">
                             Come to Twitter already this day.
@@ -41,6 +39,37 @@ const SignIn = () => {
                             <button onClick={handleClickOpen} className="link__outline">Login</button>
                         </div>
                     </div>
+                    <DialogComponent title="Login to Twitter" open={open} onClose={() => handleClickOpen()}>
+                        <FormControl component="fieldset" fullWidth>
+                            <FormGroup aria-label="position" row>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="email"
+                                    label="Enter your Email"
+                                    type="email"
+                                    InputLabelProps={{shrink: true}}
+                                    variant="filled"
+                                    fullWidth
+                                    className={classes.loginSideField}
+                                />
+                                <TextField
+                                    className={classes.loginSideField}
+                                    autoFocus
+                                    margin="dense"
+                                    id="password"
+                                    label="Enter your Password"
+                                    type="password"
+                                    InputLabelProps={{shrink: true}}
+                                    variant="filled"
+                                    fullWidth
+                                />
+                                <Button onClick={handleClickOpen} variant="contained" color="primary" fullWidth>
+                                    Login
+                                </Button>
+                            </FormGroup>
+                        </FormControl>
+                    </DialogComponent>
                 </div>
             </section>
 
