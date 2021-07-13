7533aeb0db711b3d24715f0f94e52d0e00fcd08f
Oleh <oleh.m97@gmail.com>
2021-07-13 23:41:04 +0300

    bvv

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -14,10 +14,17 @@ const useStyles = makeStyles((theme) => ({
 const SignIn = () => {
     const classes = useStyles()
     const [open, setOpen] = useState(false)
+    const [modalResistration, setModalRegistration] = useState(false)
 
-    const handleClickOpen = () => {
-        setOpen(!open)
+    const handleClickOpen = (action: string) => {
+        if (action === 'login') {
+            setOpen(prev => !prev)
+        } else {
+            setModalRegistration(prev => !prev)
+        }
+
     }
+
     return (
         <div className="container_auth">
             <section className="wrapper__left">
@@ -35,12 +42,12 @@ const SignIn = () => {
                             Come to Twitter already this day.
                         </div>
                         <div className="main__form">
-                            <button className="link__fill">Registration</button>
-                            <button onClick={handleClickOpen} className="link__outline">Login</button>
+                            <button  onClick={() => handleClickOpen('reg')} className="link__fill">Registration</button>
+                            <button onClick={() => handleClickOpen('login')} className="link__outline">Login</button>
                         </div>
                     </div>
-                    
-                    <DialogComponent title="Login to Twitter" open={open} onClose={() => handleClickOpen()}>
+
+                    <DialogComponent title="Login to Twitter" open={open} onClose={() => handleClickOpen('login')}>
                         <FormControl component="fieldset" fullWidth>
                             <FormGroup aria-label="position" row>
                                 <TextField
@@ -65,8 +72,40 @@ const SignIn = () => {
                                     variant="filled"
                                     fullWidth
                                 />
-                                <Button onClick={handleClickOpen} variant="contained" color="primary" fullWidth>
+                                <Button onClick={() => handleClickOpen('login')} variant="contained" color="primary" fullWidth>
                                     Login
+                                </Button>
+                            </FormGroup>
+                        </FormControl>
+                    </DialogComponent>
+
+                    <DialogComponent title="Registration to Twitter" open={modalResistration} onClose={() => handleClickOpen('reg')}>
+                        <FormControl component="fieldset" fullWidth>
+                            <FormGroup aria-label="position" row>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Enter your Name"
+                                    type="name"
+                                    InputLabelProps={{shrink: true}}
+                                    variant="filled"
+                                    fullWidth
+                                    className={classes.loginSideField}
+                                />
+                                <TextField
+                                    className={classes.loginSideField}
+                                    autoFocus
+                                    margin="dense"
+                                    id="email"
+                                    label="Enter your Email"
+                                    type="email"
+                                    InputLabelProps={{shrink: true}}
+                                    variant="filled"
+                                    fullWidth
+                                />
+                                <Button onClick={() => handleClickOpen('reg')} variant="contained" color="primary" fullWidth>
+                                    Next
                                 </Button>
                             </FormGroup>
                         </FormControl>
