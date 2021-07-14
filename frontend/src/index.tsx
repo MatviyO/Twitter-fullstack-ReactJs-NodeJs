a132dc9f0fe8cc70feb35045fd4ef015f69222f7
Oleh <oleh.m97@gmail.com>
2021-07-14 23:53:13 +0300

    route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 import { ThemeProvider } from '@material-ui/core';
 import theme from "./utils/theme/theme";
@@ -9,7 +10,9 @@ import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-          <App />
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
