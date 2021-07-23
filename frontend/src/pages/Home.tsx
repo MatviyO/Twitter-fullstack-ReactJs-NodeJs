0c6f84bfd91aa924d98df4f7c5957294d40c76d5
Oleh <oleh.m97@gmail.com>
2021-07-23 18:11:19 +0300

    up

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, IconButton, makeStyles, Paper, List, ListItem, Typography} from "@material-ui/core";
+import {Grid, IconButton, makeStyles, Paper, Typography, Container} from "@material-ui/core";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -9,6 +9,9 @@ import ListAltIcon from '@material-ui/icons/ListAlt';
 import PersonIcon from '@material-ui/icons/Person';
 
 const useStyles = makeStyles((theme) => ({
+    wrapper: {
+        height: '10vh'
+    },
     root: {
         flexGrow: 1,
     },
@@ -33,68 +36,71 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: 15
     },
     sideMenuListIcon: {
-        fontSize: 25,
+        fontSize: 28,
+    },
+    logoTwitter: {
+        fontSize: 36
     }
 }));
 
 const Home = () => {
     const classes = useStyles();
     return (
-        <section className={classes.root}>
+        <Container className={classes.wrapper} maxWidth="lg">
             <Grid container spacing={3}>
                 <Grid item xs={2}>
                     <ul className={classes.sideMenuList}>
                         <li className={classes.sideMenuListItem}>
                             <IconButton aria-label="delete" color="primary" className={classes.margin}>
-                                <TwitterIcon className={classes.sideMenuListIcon} />
+                                <TwitterIcon className={classes.logoTwitter}/>
                             </IconButton>
                         </li>
                         <li className={classes.sideMenuListItem}>
                             <IconButton aria-label="delete" color="default" className={classes.margin}>
-                                <SearchIcon className={classes.sideMenuListIcon} />
+                                <SearchIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
                             <Typography className={classes.sideMenuListLabel} variant="h6">Search</Typography>
                         </li>
                         <li className={classes.sideMenuListItem}>
                             <IconButton aria-label="delete" color="default" className={classes.margin}>
-                                <NotificationsIcon className={classes.sideMenuListIcon} />
+                                <NotificationsIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
-                           <Typography className={classes.sideMenuListLabel} variant="h6">Notifications</Typography>
+                            <Typography className={classes.sideMenuListLabel} variant="h6">Notifications</Typography>
                         </li>
                         <li className={classes.sideMenuListItem}>
-                            <IconButton aria-label="delete"  color="default" className={classes.margin}>
-                                <EmailIcon className={classes.sideMenuListIcon} />
+                            <IconButton aria-label="delete" color="default" className={classes.margin}>
+                                <EmailIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
-                           <Typography className={classes.sideMenuListLabel} variant="h6">Messages</Typography>
+                            <Typography className={classes.sideMenuListLabel} variant="h6">Messages</Typography>
                         </li>
                         <li className={classes.sideMenuListItem}>
-                            <IconButton aria-label="delete"  color="default" className={classes.margin}>
-                                <BookmarkIcon className={classes.sideMenuListIcon} />
+                            <IconButton aria-label="delete" color="default" className={classes.margin}>
+                                <BookmarkIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
-                           <Typography className={classes.sideMenuListLabel} variant="h6">Bookmarks</Typography>
+                            <Typography className={classes.sideMenuListLabel} variant="h6">Bookmarks</Typography>
                         </li>
                         <li className={classes.sideMenuListItem}>
-                            <IconButton aria-label="delete"  color="default" className={classes.margin}>
-                                <ListAltIcon className={classes.sideMenuListIcon} />
+                            <IconButton aria-label="delete" color="default" className={classes.margin}>
+                                <ListAltIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
-                           <Typography className={classes.sideMenuListLabel} variant="h6">List</Typography>
+                            <Typography className={classes.sideMenuListLabel} variant="h6">List</Typography>
                         </li>
                         <li className={classes.sideMenuListItem}>
-                            <IconButton aria-label="delete"  color="default" className={classes.margin}>
-                                <PersonIcon className={classes.sideMenuListIcon} />
+                            <IconButton aria-label="delete" color="default" className={classes.margin}>
+                                <PersonIcon className={classes.sideMenuListIcon}/>
                             </IconButton>
-                           <Typography className={classes.sideMenuListLabel} variant="h6">Profile</Typography>
+                            <Typography className={classes.sideMenuListLabel} variant="h6">Profile</Typography>
                         </li>
                     </ul>
                 </Grid>
                 <Grid item xs={7}>
-                    <Paper >xs</Paper>
+                    <Paper>xs</Paper>
                 </Grid>
                 <Grid item xs={3}>
-                    <Paper >xs</Paper>
+                    <Paper>xs</Paper>
                 </Grid>
             </Grid>
-        </section>
+        </Container>
     );
 };
 
